e8b77b7a0a843ed2d79f2c365682b6882e9b9c3c
Sagor <mbrsagor@gmail.com>
2020-07-31 14:38:03 +0600

    Typescript first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var first_name = "mbr";
+var last_name = "sagor";
+var full_name = first_name + " " + last_name;
+console.log(full_name);
+console.log("hello sagor, how about you?");
+console.log("I'm full-stack software engineer...");
---
+++ index.ts
@@ -0,0 +1,9 @@
+var first_name: string = "mbr";
+var last_name: string = "sagor";
+var full_name: string = first_name + " " + last_name;
+
+console.log(full_name);
+
+console.log("hello sagor, how about you?");
+console.log("I'm full-stack software engineer...");
+
