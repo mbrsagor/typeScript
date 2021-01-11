a279d90fca46bdd3709dc08ffcacdfd0ba0ab006
Sagor <mbrsagor@gmail.com>
2021-01-11 23:37:50 +0600

    How does only string array work

---
+++ new/array.js
@@ -0,0 +1,7 @@
+var a = [];
+a.push("Sagor");
+a.push("Ohi");
+a.push("Meg");
+a.push("Dhurbo");
+a.push("Shanto");
+console.log(a);
---
+++ new/array.ts
@@ -0,0 +1,9 @@
+let a: string[] = [];
+
+a.push("Sagor");
+a.push("Ohi");
+a.push("Meg");
+a.push("Dhurbo");
+a.push("Shanto");
+
+console.log(a);
