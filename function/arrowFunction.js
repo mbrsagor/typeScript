b916d695b49ecd75597fcc92520ad6935c104ff2
mbrsagor <mbrsagor@gmail.com>
2021-04-14 20:18:13 +0600

    Function with argument

--- function/arrowFunction.js
+++ function/arrowFunction.js
@@ -4,3 +4,8 @@ var sum = function (num, num2) {
 console.log(sum(30, 20));
 var say = function () { return console.log("Hey, I'm arrow function"); };
 say();
+function say_something(name) {
+    return "Hello thre, this is " + name;
+}
+var _name = say_something("Sagor!");
+console.log(_name);
--- function/arrowFunction.ts
+++ function/arrowFunction.ts
@@ -7,3 +7,11 @@ console.log(sum(30, 20));
 let say = () => console.log("Hey, I'm arrow function")
 say();
 
+
+
+function say_something(name: string){
+    return `Hello thre, this is ${name}`
+}
+
+let _name = say_something("Sagor!");
+console.log(_name);
